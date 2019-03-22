0d543a42cfac753cecf4fa713df411fb2c87da5b
drekid2 <drekid2@gmail.com>
2019-03-22 19:46:29 -0400

    Implemented a binding in C++ to bind code to the blue print widget for the painting grid inside of the widget.

---
+++ LightPainter/Source/LightPainter/UI/PaintingPicker/PaintingGrid.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PaintingGrid.h"
+
+void UPaintingGrid::AddPainting()
+{
+	if (PaintingGrid)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Grid name: %s"), *PaintingGrid->GetName());
+	}
+}
